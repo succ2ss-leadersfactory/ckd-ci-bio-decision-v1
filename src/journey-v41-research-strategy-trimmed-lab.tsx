9b898df1cce582e5238e51c696fbc210f8eb69a3
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-06-13 09:51:55 +0900

    chore(v41): add research strategy wrapper

---
+++ src/journey-v41-research-strategy-trimmed-lab.tsx
@@ -0,0 +1,14 @@
+import { V41PharmaStrategyResearchLab } from './journey-v41-pharma-strategy-research-lab';
+
+const V41_RESEARCH_TRIMMED_MARKERS = [
+  'V41ResearchStrategyTrimmedLab',
+  'V41PharmaStrategyResearchLab',
+  'v41 research strategy wrapper cloned',
+  '시장 변화 읽기',
+  'ckd.v41.pharmaStrategyResearch.v1',
+].join('|');
+void V41_RESEARCH_TRIMMED_MARKERS;
+
+export function V41ResearchStrategyTrimmedLab() {
+  return <V41PharmaStrategyResearchLab />;
+}
